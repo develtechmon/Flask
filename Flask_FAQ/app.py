c5b843e940bf6455e51a27f6a07c4ba0be0e8578
Captluke2328 <81543946+Captluke2328@users.noreply.github.com>
2022-11-11 05:03:44 +0800

    + Added main form

--- Flask_FAQ/app.py
+++ Flask_FAQ/app.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 def base():
     return render_template('home.html')
 
-@app.route('/search/')
+@app.route('/search')
 def search():
     return render_template('search.html')
 
-@app.route('/entry/')
+@app.route('/entry')
 def entry():
     return render_template('entry.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run()

2bc2f5065d8012f10411ca961b10040ef0f61dbb
Captluke2328 <81543946+Captluke2328@users.noreply.github.com>
2022-11-10 20:00:36 +0800

    + added Flask FAQ project

---
+++ Flask_FAQ/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def base():
+    return render_template('home.html')
+
+@app.route('/search/')
+def search():
+    return render_template('search.html')
+
+@app.route('/entry/')
+def entry():
+    return render_template('entry.html')
+
+if __name__ == '__main__':
+    app.run()

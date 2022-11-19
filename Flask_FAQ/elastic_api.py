755cd65a206aee6ceda3c20062375bc3f37a4ce7
Captluke2328 <81543946+Captluke2328@users.noreply.github.com>
2022-11-19 13:39:32 +0800

    + added or and add condition

--- Flask_FAQ/elastic_api.py
+++ Flask_FAQ/elastic_api.py
@@ -17,8 +17,62 @@ class elastic:
         
         if self.es.ping(): print("Connected to server")  
         else: print("Failed to connect")
-            
+        
     def search(self,data):
+        operator = []
+        operator.append(data)
+        
+        print(data)
+        for x in operator:
+            y = x.split()
+            
+            if 'and:' in y:      
+                y.remove('and:')
+                print(y)
+                
+                for z in y:
+                    print(z)
+
+                    self.search_and(z)
+            
+            else:
+                self.search_or(data)
+            
+        # print(data)
+        # x = data.split()
+        # for i in x:
+        #     if 'and:' in i:
+        #         x.remove('and:')
+              
+        #         print(x)
+                #print(x[1:])
+                #self.search_and(data)
+                
+    def search_or(self,data):
+        # See Link https://www.elastic.co/guide/en/elasticsearch/client/python-api/current/examples.html
+        # See link https://dylancastillo.co/elasticsearch-python/
+        print("OR")
+        resp = self.es.search(
+        index = "hotlinefaq",
+        body={     
+            "query": {
+            "multi_match": {
+            "query": data,
+            "fields": ["Process", "Customer", "Subject", "Description", "Topic"],
+            "operator": "or",
+            "type": "cross_fields"
+            },
+        },    
+        })
+                
+        jsonString = json.dumps(resp['hits']['hits'])
+        jsonFile = open("D:\Flask_Tutorial\Flask_FAQ\static\json\data.json","w")
+        jsonFile.write(jsonString)
+        jsonFile.close()
+        
+    
+    def search_and(self,data):
+        print("AND")
         # See Link https://www.elastic.co/guide/en/elasticsearch/client/python-api/current/examples.html
         # See link https://dylancastillo.co/elasticsearch-python/
         resp = self.es.search(
@@ -28,7 +82,8 @@ class elastic:
             "multi_match": {
             "query": data,
             "fields": ["Process", "Customer", "Subject", "Description", "Topic"],
-            "operator": "or",
+            "operator": "and",
+            "type": "cross_fields"
             },
         },    
         })
@@ -87,13 +142,15 @@ if __name__ == "__main__":
     init = elastic()
     init.connect()
     
-    #init.search("XT018")
+    init.search('"XP018" "ESD"')
+    
+    #init.search_and("XR013")
     
     #init.update("00","xt018","lukas","esd","esd clamp","guideline","refer to guideline")
     
     #init.all()
     
-    init.add("00","xt018","james","esd","esd clamp","guideline","refer to guideline")
+    #init.add("00","xt018","james","esd","esd clamp","guideline","refer to guideline")
 
         
         

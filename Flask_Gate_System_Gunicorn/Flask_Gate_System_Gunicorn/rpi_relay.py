adcd4a92053b2cfa4e601c84a4e7fe352199e9af
Captluke2328 <125783320+develtechmon@users.noreply.github.com>
2023-02-28 11:04:51 +0800

    +added flask for gunicorn deployment

---
+++ Flask_Gate_System_Gunicorn/Flask_Gate_System_Gunicorn/rpi_relay.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import time
+
+def rpi_relay(state):
+    GPIO.setmode(GPIO.BCM)
+    
+    # Close the gate
+    GPIO.setup(14,GPIO.OUT)
+
+    # Pause and Close the gate
+    GPIO.setup(15,GPIO.OUT)
+    
+    GPIO.output(14, GPIO.LOW)
+    GPIO.output(15, GPIO.LOW)
+    
+    print(state)
+    try:
+        if state == "open":
+            GPIO.output(14, GPIO.HIGH)
+            GPIO.output(15, GPIO.LOW)
+            print('Relay 1 ON')
+            time.sleep(1)
+        
+        elif state == "pause":
+            GPIO.output(15, GPIO.HIGH)
+            GPIO.output(14, GPIO.LOW)
+            print('Relay 2 ON')
+            time.sleep(1)
+            
+        
+        elif state == "close":
+            GPIO.output(15, GPIO.HIGH)
+            GPIO.output(14, GPIO.LOW)
+            print('Relay 2 ON')
+            time.sleep(1)
+    except:
+        pass
+    
+    finally:
+        GPIO.cleanup()
+         
